7d7db07c338b749d88bd8fda0e40a3c9b7bd169f
Kayan Florencio <kayanf.silva@outlook.com>
2021-04-04 22:02:14 -0300

    Aula 8 - Finalizando curso

--- Collections/aula06e07e08/main.py
+++ Collections/aula06e07e08/main.py
@@ -1,4 +1,6 @@
 from operator import attrgetter
+from functools import total_ordering
+
 idades = [15, 87, 32, 65, 56, 32, 49, 37]
 print(sorted(idades))  # ordenando do menor para o maior
 print(list(reversed(idades)))  # ordenando ao contrario
@@ -10,6 +12,7 @@ nomes = ["Guilherme", "Daniela", "Paulo"]
 print(sorted(nomes))
 
 
+@total_ordering
 class ContaSalario:
     def __init__(self, codigo):
         self._codigo = codigo
@@ -22,7 +25,10 @@ class ContaSalario:
         return self._codigo == outro._codigo and self._saldo == outro._saldo
 
     def __lt__(self, outro):
-        return self._saldo < outro._saldo
+        if self._saldo != outro._saldo:
+            return self._saldo < outro._saldo
+
+        return self._codigo < outro._codigo
 
     def deposita(self, valor):
         self._saldo += valor
@@ -40,7 +46,10 @@ conta_da_daniela.deposita(1000)
 conta_do_paulo = ContaSalario(183)
 conta_do_paulo.deposita(510)
 
-contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+conta_do_kayan = ContaSalario(200)
+conta_do_kayan.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo, conta_do_kayan]
 
 
 def extrai_saldo(conta):
@@ -54,6 +63,20 @@ for conta in sorted(contas, key=extrai_saldo):
 for conta in sorted(contas, key=attrgetter("_saldo")):
     print(conta)
 
-print(conta_do_guilherme < conta_da_daniela)
+# utilizando criterio de desempate
+for conta in sorted(contas, key=attrgetter("_saldo", "_codigo")):
+    print(conta)
+
 for conta in sorted(contas):
     print(conta)
+
+print("O saldo do Guilherme é menor do que da Daniela:",
+      conta_do_guilherme < conta_da_daniela, "\n")
+
+# utilizando functools para ordenações
+print("O saldo do Guilherme é menor ou igual do que da Daniela:",
+      conta_do_guilherme <= conta_da_daniela, "\n")
+print("O saldo do Guilherme é menor ou igual do que do Kayan:",
+      conta_do_guilherme <= conta_do_kayan, "\n")
+print("O saldo do Guilherme é igual a do Guilherme:",
+      conta_do_guilherme == conta_do_guilherme, "\n")
