91565789cd9da3272a43b14d08dfebb24228aafe
Kayan Florencio <kayanf.silva@outlook.com>
2021-05-04 22:06:35 -0300

    Validação de telefone

--- ValidaçõesBrasileiras/main.py
+++ ValidaçõesBrasileiras/main.py
@@ -1,4 +1,14 @@
-from cpf import cpf
+from telefonesBr import TelefonesBr
+import re
 
-cpf_um = cpf("15316264754")
-print(cpf_um)
+padrao = "[0-9][a-z][0-9]"
+texto = "123 1a2 1cc aa1"
+resposta = re.search(padrao, texto)
+
+print(resposta)
+
+
+telefone = "552126481234"
+
+telefone_objeto = TelefonesBr(telefone)
+print(telefone_objeto)
