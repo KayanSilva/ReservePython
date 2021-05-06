3f436c6aee9cbd1aab303c539bb2c151676f7a90
Kayan Florencio <kayanf.silva@outlook.com>
2021-05-05 22:54:18 -0300

    Adicionando pasta para TDD

--- ValidaçõesBrasileiras/main.py
+++ ValidaçõesBrasileiras/main.py
@@ -1,4 +1,7 @@
+from acesso_cep import BuscaEndereco
+from cpf_cnpj import DocCpf
 from telefonesBr import TelefonesBr
+from datas_br import DatasBr
 import re
 
 padrao = "[0-9][a-z][0-9]"
@@ -12,3 +15,14 @@ telefone = "552126481234"
 
 telefone_objeto = TelefonesBr(telefone)
 print(telefone_objeto)
+
+
+cadastro = DatasBr()
+print(cadastro.dia_semana())
+print(cadastro)
+
+
+cep = "25800320"
+objeto_cep = BuscaEndereco(cep)
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
