e412a7448dea914f01434d1e74ab634ef6670daf
Kayan Florencio <kayanf.silva@outlook.com>
2021-12-23 00:21:45 -0300

    Melhorando a gestão com o with open

--- IO/principal.py
+++ IO/principal.py
@@ -1,13 +1,9 @@
 try:
-    arquivos_contatos = open('dados/nao-existe.csv', encoding='latin_1', mode='w+')
-
-    for linha in arquivos_contatos:
-        print(linha, end='')
+    with open('dados/contatos.csv', encoding='latin_1') as arquivos_contatos:
+        for linha in arquivos_contatos:
+            print(linha, end='')
 
 except FileNotFoundError:
     print('Arquivo não encontrado')
 except PermissionError:
     print('Sem permissão de escrita')
-
-finally:
-    arquivos_contatos.close()
